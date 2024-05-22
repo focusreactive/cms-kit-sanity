674652146d7bb76ea8c2753e6c4e168fa11197fb
Oleg Proskurin <regx@usul.su>
2024-05-22 21:15:30 +0700

    Fix

--- src/sets/config.ts
+++ src/sets/config.ts
@@ -2,5 +2,4 @@ import { twBase } from './tw-base/sa-set';
 import { twExt } from './tw-ext/sa-set';
 import { fbTest } from './fb-test/sa-set';
 
-// export const sets = [fbTest];
 export const sets = [twBase, twExt, fbTest];
