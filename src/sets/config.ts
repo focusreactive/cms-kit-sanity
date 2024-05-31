a15907aa5c4956bc9ad8758545f26bbdf8697226
Oleg Proskurin <regx@usul.su>
2024-05-31 16:47:26 +0700

    Disable FB set

--- src/sets/config.ts
+++ src/sets/config.ts
@@ -1,5 +1,6 @@
 import { twBase } from './tw-base/sa-set';
 import { twExt } from './tw-ext/sa-set';
-import { fbTest } from './fb-test/sa-set';
+// import { fbTest } from './fb-test/sa-set';
 
-export const sets = [twBase, twExt, fbTest];
+// export const sets = [twBase, twExt, fbTest];
+export const sets = [twBase, twExt];
