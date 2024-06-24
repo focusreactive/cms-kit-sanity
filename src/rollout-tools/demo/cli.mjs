c6151057c77a06b7686bca48d54cda73c09ea1bd
Oleg Proskurin <regx@usul.su>
2024-06-24 17:40:08 +0700

    Add script

--- src/rollout-tools/demo/cli.mjs
+++ src/rollout-tools/demo/cli.mjs
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-import { localFlow } from './localFlow.mjs';
+import fetch from 'node-fetch';
+import { exec } from 'child_process';
+
 import {
-  fetchSanityOrganizations,
-  fetchSanityUserInfo,
-} from '../local/fetchSanityOrganizations.mjs';
-import {
-  fetchVercelTeams,
-  fetchVercelUserInfo,
-} from '../local/fetchVercelTeams.mjs';
-import ora from 'ora';
+  createSanityProject,
+  createSanityReadToken,
+  createVercelProject,
+  getVercelProjects,
+} from '../local/services.mjs';
 
 if (process.env.GITHUB_ACTIONS !== 'true') {
   console.warn(
@@ -18,17 +17,28 @@ if (process.env.GITHUB_ACTIONS !== 'true') {
   process.exit(1);
 }
 
-// Check required environment variables
-const requiredEnvVars = [
-  'VERCEL_PERSONAL_AUTH_TOKEN',
-  'SANITY_PERSONAL_AUTH_TOKEN',
-  'SANITY_ORGANIZATION_ID',
-  'VERCEL_FR_TEAM_ID',
-  'PROJECT_NAME',
-  'NEXT_PUBLIC_SANITY_DATASET',
-  'MAX_NUMBER_OF_PROJECTS',
-  'EMAIL',
-];
+const secrets = {
+  VERCEL_PERSONAL_AUTH_TOKEN: process.env.VERCEL_PERSONAL_AUTH_TOKEN,
+  SANITY_PERSONAL_AUTH_TOKEN: process.env.SANITY_PERSONAL_AUTH_TOKEN,
+  SANITY_AUTH_TOKEN: process.env.SANITY_PERSONAL_AUTH_TOKEN,
+  ROLL_OUT_API_TOKEN: process.env.ROLL_OUT_API_TOKEN,
+};
+
+const inputs = {
+  EMAIL: process.env.EMAIL,
+  SANITY_ORGANIZATION_ID: process.env.SANITY_ORGANIZATION_ID,
+  VERCEL_FR_TEAM_ID: process.env.VERCEL_FR_TEAM_ID,
+  PROJECT_NAME: process.env.PROJECT_NAME,
+  PROJECT_PREFIX: process.env.PROJECT_PREFIX,
+  MAX_NUMBER_OF_PROJECTS: process.env.MAX_NUMBER_OF_PROJECTS,
+  NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  REPO_ID: process.env.REPO_ID,
+  REPO_PROD_BRANCH: process.env.REPO_PROD_BRANCH,
+  REPO_TYPE: process.env.REPO_TYPE,
+};
+
+const requiredEnvVars = Object.keys({ ...secrets, ...inputs });
 
 requiredEnvVars.forEach((envVar) => {
   if (!process.env[envVar]) {
@@ -37,70 +47,361 @@ requiredEnvVars.forEach((envVar) => {
   }
 });
 
-// Extract necessary environment variables
-const email = process.env.EMAIL;
-const sanityProjectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '';
-const sanityDatasetName = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const vercelProjectName = process.env.PROJECT_NAME;
-const vercelToken = process.env.VERCEL_PERSONAL_AUTH_TOKEN;
-const sanityToken = process.env.SANITY_PERSONAL_AUTH_TOKEN;
 
-// Fetch user and team information
-const vercelUserInfo = await fetchVercelUserInfo(vercelToken);
-const vercelTeams = await fetchVercelTeams(vercelToken);
-const sanityUserInfo = await fetchSanityUserInfo(sanityToken);
-const sanityOrganizations = await fetchSanityOrganizations(sanityToken);
+async function localRollout({ inputs, secrets }) {
+  const {
+    EMAIL: email,
+    PROJECT_NAME: projectName,
+    PROJECT_PREFIX: projectPrefix,
+    NEXT_PUBLIC_SANITY_DATASET: datasetName,
+    VERCEL_FR_TEAM_ID: selectedTeam,
+    SANITY_ORGANIZATION_ID: selectedOrg,
+  } = inputs;
 
-// Ensure necessary data is present
-if (!sanityProjectId) {
-  console.error('Sanity project ID is missing');
-  process.exit(1);
+  const summary = {};
+
+  try {
+    if (!(email && isValidEmail(email))) {
+      console.error('Email is not valid');
+      return;
+    }
+
+    const username = email
+      .split('@')[0]
+      .toLowerCase()
+      .replace(/[^a-z0-9]/g, '') // prevent forbidden symbols
+      .replace(/\./g, '') // prevent forbidden symbols
+      .slice(0, 50); // prevent project name from being too long
+    const finalProjectName = `${projectPrefix}-${username}-${projectName}`;
+
+    summary.username = username;
+    summary.email = email;
+    summary.finalProjectName = finalProjectName;
+
+    console.log('Username: ' + username);
+    console.log('Email: ' + email);
+    console.log('Final Project Name (Sanity): ' + finalProjectName);
+
+    console.log('Fetching existing Vercel projects...');
+    const existingProjects = await getVercelProjects();
+    console.log('Fetched existing Vercel projects.');
+
+    const allowToCreateProject =
+      existingProjects &&
+      existingProjects.length < parseInt(secrets.MAX_NUMBER_OF_PROJECTS || '5');
+
+    const existingProject = existingProjects?.find(
+      (project) => project.name === finalProjectName,
+    );
+
+    if (!allowToCreateProject) {
+      console.error('Limit of the projects reached');
+      return;
+    }
+    if (existingProject) {
+      console.error('Project with this email already exists');
+      return;
+    }
+
+    console.log('Creating Sanity project...');
+    const sanityProjectId = await createSanityProject(finalProjectName);
+
+    if (!sanityProjectId) {
+      console.error('Failed to create Sanity project.');
+      process.exit(1);
+    }
+    console.log('Sanity project created...');
+
+    summary.sanityProjectId = sanityProjectId;
+    console.log('Sanity Project ID: ' + sanityProjectId);
+
+    console.log('Creating Sanity read token...');
+    const sanityReadToken = await createSanityReadToken(sanityProjectId);
+    console.log('Sanity read token created.');
+
+    console.log('Creating Vercel project...');
+    const projectData = await createVercelProject({
+      sanityReadToken: sanityReadToken,
+      projectName: finalProjectName,
+      sanityProjectId,
+      sanityDatasetName: datasetName,
+    });
+
+    if (!projectData?.projectId) {
+      console.error('Failed to create Vercel project.');
+      process.exit(1);
+    }
+    console.log('Vercel project created.');
+
+    summary.projectName = projectData.projectName;
+    summary.projectId = projectData.projectId;
+    summary.deploymentUrl = projectData.deploymentUrl;
+    summary.studioUrl = `${projectData.deploymentUrl}/admin`;
+    summary.vercelUrl = `https://vercel.com/${selectedTeam.slug}/${projectData.projectName}`;
+    summary.sanityUrl = `https://www.sanity.io/organizations/${selectedOrg.id}/project/${sanityProjectId}`;
+    summary.datasetName = datasetName;
+    summary.REPO_NAME = secrets.REPO_NAME;
+
+    console.log('Vercel Project Name: ' + projectData.projectName);
+    console.log('Vercel Project ID: ' + projectData.projectId);
+    console.log('Deployment URL: ' + projectData.deploymentUrl);
+    console.log('Sanity Studio: ' + summary.studioUrl);
+    console.log('Sanity Dataset Name: ' + datasetName);
+    console.log('Repo Name: ' + secrets.REPO_NAME);
+
+    console.log('Starting local flow...\n\n');
+
+    // Step 1: Add envs to Vercel project
+    console.log('Adding environment variables to Vercel project...');
+    try {
+      await fetch(
+        `https://api.vercel.com/v10/projects/${projectData.projectId}/env?teamId=${secrets.VERCEL_FR_TEAM_ID}`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${secrets.VERCEL_PERSONAL_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            key: 'VERCEL_PROJECT_ID',
+            value: projectData.projectId,
+            type: 'encrypted',
+            target: ['production', 'preview', 'development'],
+          }),
+        },
+      );
+
+      await fetch(
+        `https://api.vercel.com/v10/projects/${projectData.projectId}/env?teamId=${secrets.VERCEL_FR_TEAM_ID}`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${secrets.VERCEL_PERSONAL_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            key: 'VERCEL_PROJECT_NAME',
+            value: projectData.projectName,
+            type: 'encrypted',
+            target: ['production', 'preview', 'development'],
+          }),
+        },
+      );
+      console.log('Environment variables added to Vercel project.');
+    } catch (error) {
+      console.error(
+        'Failed to add environment variables to Vercel project.',
+        error,
+      );
+      throw error;
+    }
+
+    // Step 2: Add Sanity CORS entry
+    console.log('Adding Sanity CORS entry...');
+    try {
+      await fetch(
+        `https://api.sanity.io/v2021-06-07/projects/${sanityProjectId}/cors`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${secrets.SANITY_PERSONAL_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            origin: projectData.deploymentUrl,
+            allowCredentials: true,
+          }),
+        },
+      );
+
+      await fetch(
+        `https://api.sanity.io/v2021-06-07/projects/${sanityProjectId}/cors`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${secrets.SANITY_PERSONAL_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            origin: 'http://localhost:3000/',
+            allowCredentials: true,
+          }),
+        },
+      );
+      console.log('Sanity CORS entry added.');
+    } catch (error) {
+      console.error('Failed to add Sanity CORS entry.', error);
+      throw error;
+    }
+
+    // Step 3: Invite user to Sanity project
+    console.log('Inviting user to Sanity project...');
+    try {
+      await fetch(
+        `https://api.sanity.io/v2021-06-07/invitations/project/${sanityProjectId}`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${secrets.SANITY_PERSONAL_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: email,
+            role: 'editor',
+          }),
+        },
+      );
+      console.log('User invited to Sanity project.');
+    } catch (error) {
+      console.error('Failed to invite user to Sanity project.', error);
+      throw error;
+    }
+
+    // Step 4: Create a new Sanity dataset
+    console.log('Creating a new Sanity dataset...');
+    try {
+      await fetch(
+        `https://api.sanity.io/v2021-06-07/projects/${sanityProjectId}/datasets/${datasetName}`,
+        {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${secrets.SANITY_PERSONAL_AUTH_TOKEN}`,
+          },
+        },
+      );
+      console.log('New Sanity dataset created.');
+    } catch (error) {
+      console.error('Failed to create a new Sanity dataset.', error);
+      throw error;
+    }
+
+    // Step 5: Fill the dataset with data from prod-copy.tar.gz
+    console.log('Filling the dataset with data from prod-copy.tar.gz...');
+    try {
+      await new Promise((resolve, reject) => {
+        exec(
+          `SANITY_AUTH_TOKEN=${secrets.SANITY_PERSONAL_AUTH_TOKEN} npx sanity dataset import initial-data.tar.gz ${datasetName}`,
+          (error, stdout, stderr) => {
+            if (error) {
+              console.error(`Error importing dataset: ${stderr}`);
+              return reject(error);
+            }
+            console.log(`Dataset imported: ${stdout}`);
+            resolve();
+          },
+        );
+      });
+    } catch (error) {
+      console.error('Failed to fill the dataset with data.', error);
+      throw error;
+    }
+
+    // Step 6: Add deploy hook(/api/roll-out/deploy) to Sanity project
+    console.log('Adding deploy hook to Sanity project...');
+    try {
+      await fetch(
+        `https://api.sanity.io/v2021-10-04/hooks/projects/${sanityProjectId}`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${secrets.SANITY_PERSONAL_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            type: 'document',
+            name: 'Sanity Studio',
+            url: `https://${projectData.projectName}.vercel.app/api/roll-out/deploy`,
+            httpMethod: 'POST',
+            apiVersion: 'v2021-03-25',
+            includeDrafts: false,
+            dataset: '*',
+            rule: {
+              on: ['create', 'update', 'delete'],
+            },
+            headers: {
+              Authorization: `Bearer ${secrets.ROLL_OUT_API_TOKEN}`,
+            },
+          }),
+        },
+      );
+      console.log('Deploy hook added to Sanity project.');
+    } catch (error) {
+      console.error('Failed to add deploy hook to Sanity project.', error);
+      throw error;
+    }
+
+    // Step 7: Create a new Vercel deployment
+    console.log('Creating a new Vercel deployment...');
+    try {
+      const body = {
+        name: projectData.projectName,
+        project: projectData.projectId,
+        target: 'production',
+        gitSource: {
+          repoId: secrets.REPO_ID,
+          ref: secrets.REPO_PROD_BRANCH,
+          type: 'github',
+        },
+      };
+      const response = await fetch(
+        `https://api.vercel.com/v13/deployments?teamId=${secrets.VERCEL_FR_TEAM_ID}`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${secrets.VERCEL_PERSONAL_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(body),
+        },
+      );
+      const data = await response.json();
+      console.log('New Vercel deployment created.');
+    } catch (error) {
+      console.error('Failed to create a new Vercel deployment.', error);
+      throw error;
+    }
+
+    console.log('All steps were successful 🎉\n\n');
+    return summary;
+  } catch (error) {
+    console.error('One of the steps was not successful 😿');
+    console.log(JSON.stringify(summary, null, 2));
+    return null;
+  }
 }
 
-// Define secrets and inputs for localFlow
-const secrets = {
-  VERCEL_FR_TEAM_ID: process.env.VERCEL_FR_TEAM_ID,
-  VERCEL_PERSONAL_AUTH_TOKEN: process.env.VERCEL_PERSONAL_AUTH_TOKEN,
-  SANITY_PERSONAL_AUTH_TOKEN: process.env.SANITY_PERSONAL_AUTH_TOKEN,
-  SANITY_ORGANIZATION_ID: process.env.SANITY_ORGANIZATION_ID,
-  ROLL_OUT_API_TOKEN: process.env.ROLL_OUT_API_TOKEN,
-  REPO_ID: process.env.REPO_ID,
-  REPO_PROD_BRANCH: process.env.REPO_PROD_BRANCH,
-  REPO_TYPE: process.env.REPO_TYPE,
-};
+// async function localFlow({ inputs, secrets }) {
+//   const vercelProjectId = inputs['vercel-project-id'] || '';
+//   const vercelProjectName = inputs['vercel-project-name'];
+//   const sanityProjectId = inputs['sanity-project-id'];
+//   const sanityDatasetName = inputs['sanity-dataset-name'];
+//   const vercelDeploymentUrl = inputs['vercel-deployment-url'];
+//   const email = inputs['email'];
 
-const inputs = {
-  email,
-  'sanity-project-id': sanityProjectId,
-  'sanity-dataset-name': sanityDatasetName,
-  'vercel-project-name': vercelProjectName,
-  'vercel-project-id': '',
-  'vercel-deployment-url': '',
-};
+//   return true;
+// }
 
-const main = async () => {
+async function main() {
   try {
-    const summary = await localFlow({ inputs, secrets });
+    const summary = await localRollout({ inputs, secrets });
 
     if (summary) {
-      console.table(summary);
-      ora().succeed(
+      console.log(
         'All projects have been successfully set up and configured! 🎉\n',
       );
-      ora().info('Here are the details:');
+      console.log('Here are the details:');
       console.log(`- Deployed website: ${summary.deploymentUrl}`);
       console.log(`- Sanity Studio: ${summary.studioUrl}`);
       console.log(`- Vercel Project: ${summary.vercelUrl}`);
       console.log(`- Sanity Project: ${summary.sanityUrl}\n`);
 
-      ora().info('The next commands you should use:');
+      console.log('The next commands you should use:');
       console.log('> pnpm run build - to build the project locally');
       console.log('> pnpm run dev - to start the dev server');
     }
   } catch (error) {
-    ora().fail('An error occurred');
-    console.error(error);
+    console.error('An error occurred', error);
   }
-};
+}
 
 main();
