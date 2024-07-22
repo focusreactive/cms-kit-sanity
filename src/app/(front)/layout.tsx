5758bd0cb2828a2f1cb6671c228187d1bc8bed4f
Oleg Proskurin <regx@usul.su>
2024-07-22 14:15:22 +0700

    Fix layout

--- src/app/(front)/layout.tsx
+++ src/app/(front)/layout.tsx
@@ -1,26 +1,26 @@
-import '@/styles/index.css'
+import '@/styles/index.css';
 
-import type { Metadata, Viewport } from 'next'
-import dynamic from 'next/dynamic'
-import { draftMode } from 'next/headers'
+import type { Metadata, Viewport } from 'next';
+import dynamic from 'next/dynamic';
+import { draftMode } from 'next/headers';
 // TODO: use this if your description is Formatted text
 // import { toPlainText } from 'next-sanity'
-import { Suspense } from 'react'
+import { Suspense } from 'react';
 
-import { urlForOpenGraphImage } from '@/sanity/lib/utils'
-import { loadHomePage, loadSettings } from '@/sanity/loader/loadQuery'
+import { urlForOpenGraphImage } from '@/sanity/lib/utils';
+import { loadHomePage, loadSettings } from '@/sanity/loader/loadQuery';
 
 const LiveVisualEditing = dynamic(
   () => import('@/sanity/loader/LiveVisualEditing'),
-)
+);
 
 export async function generateMetadata(): Promise<Metadata> {
   const [{ data: settings }, { data: homePage }] = await Promise.all([
     loadSettings(),
     loadHomePage(),
-  ])
+  ]);
 
-  const ogImage = urlForOpenGraphImage(settings?.ogImage)
+  const ogImage = urlForOpenGraphImage(settings?.ogImage);
   return {
     title: homePage?.title
       ? {
@@ -35,26 +35,22 @@ export async function generateMetadata(): Promise<Metadata> {
     openGraph: {
       images: ogImage ? [ogImage] : [],
     },
-  }
+  };
 }
 
 export const viewport: Viewport = {
   themeColor: '#000',
-}
+};
 
 export default async function IndexRoute({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <>
-      <div className="flex min-h-screen flex-col bg-white text-black">
-        <div className="mt-20 flex-grow px-4 md:px-16 lg:px-32">
-          <Suspense>{children}</Suspense>
-        </div>
-      </div>
+      <Suspense>{children}</Suspense>
       {draftMode().isEnabled && <LiveVisualEditing />}
     </>
-  )
+  );
 }
